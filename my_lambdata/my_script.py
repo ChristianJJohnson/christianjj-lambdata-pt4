aec14baf41d4d2ff3a5d12dabdfb5b2ee622f9cc
ChristianJJohnson <christianjerelj@icloud.com>
2020-03-11 12:41:32 -0500

    updated

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,10 +1,7 @@
 # Yup
 import pandas as pd
 
-from my_lambdata.my_mod import enlarge
-
 print('Hello Lambda! I know a lot of this already but practice makes perfect!')
 
 df = pd.DataFrame({"x":[1,2,3,4,5], "y":[2,4,6,8,10]})
 print(df.head())
-print(f'X:{x} X Enlarged:{enlarge(x)}')
